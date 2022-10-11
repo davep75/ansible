6603fdff71dadb559faca77e7e9ef9608a1ac430
davep75 <podolske@motorola.com>
2022-10-11 16:58:03 -0500

    added show warnings

--- source/rebuild-salt.sql
+++ source/rebuild-salt.sql
@@ -1,6 +1,11 @@
 TRUNCATE TABLE salt;
 LOAD DATA LOCAL INFILE '/home/podolske/cmdb/rhel-10-10.csv' INTO TABLE salt FIELDS TERMINATED BY ',' (minion, kernel, hostname, version);
+SHOW WARNINGS;
 LOAD DATA LOCAL INFILE '/home/podolske/cmdb/ubuntu-10-10.csv' INTO TABLE salt FIELDS TERMINATED BY ',' (minion, kernel, hostname, version);
+SHOW WARNINGS;
 LOAD DATA LOCAL INFILE '/home/podolske/cmdb/carrier-blurdev.csv' INTO TABLE salt FIELDS TERMINATED BY ',' (minion, kernel, hostname, version);
+SHOW WARNINGS;
 LOAD DATA LOCAL INFILE '/home/podolske/cmdb/carrier-coresite.csv' INTO TABLE salt FIELDS TERMINATED BY ',' (minion, kernel, hostname, version);
+SHOW WARNINGS;
 LOAD DATA LOCAL INFILE '/home/podolske/cmdb/carrier-reston.csv' INTO TABLE salt FIELDS TERMINATED BY ',' (minion, kernel, hostname, version);
+SHOW WARNINGS;
