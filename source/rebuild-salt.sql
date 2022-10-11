d32f9e760075c895bcc89348874dc346f3a73344
davep75 <podolske@motorola.com>
2022-10-11 14:01:52 -0500

    populate salt table in cmdb database

--- source/rebuild-salt.sql
+++ source/rebuild-salt.sql
@@ -1,3 +1,6 @@
 TRUNCATE TABLE salt;
 LOAD DATA LOCAL INFILE '/home/podolske/cmdb/rhel-10-10.csv' INTO TABLE salt FIELDS TERMINATED BY ',' (minion, kernel, hostname, version);
 LOAD DATA LOCAL INFILE '/home/podolske/cmdb/ubuntu-10-10.csv' INTO TABLE salt FIELDS TERMINATED BY ',' (minion, kernel, hostname, version);
+LOAD DATA LOCAL INFILE '/home/podolske/cmdb/carrier-blurdev.csv' INTO TABLE salt FIELDS TERMINATED BY ',' (minion, kernel, hostname, version);
+LOAD DATA LOCAL INFILE '/home/podolske/cmdb/carrier-coresite.csv' INTO TABLE salt FIELDS TERMINATED BY ',' (minion, kernel, hostname, version);
+LOAD DATA LOCAL INFILE '/home/podolske/cmdb/carrier-reston.csv' INTO TABLE salt FIELDS TERMINATED BY ',' (minion, kernel, hostname, version);
