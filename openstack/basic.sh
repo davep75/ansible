cb774305c822cb6cbd6115ddf868e65ce2ee8896
davep75 <podolske@motorola.com>
2023-01-23 15:53:09 -0600

    execute on nova server

--- openstack/basic.sh
+++ openstack/basic.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /usr/bin/apt-get -y install bridge-utils debootstrap ifenslave ifenslave-2.6 lsof lvm2 ntpdate openssh-server sudo tcpdump vlan python3 openvswitch-switch build-essential python3-dev ipxe-qemu python3-virtualenv lldpd libstdc++5 libxrender1 libxtst6 libxi6 x11-common python2.7-minimal numactl python3-pkgconfig cgroup-lite icu-devtools libicu-dev apt-transport-https cmake-data cmake libsystemd-dev ubuntu-cloud-keyring
-/bin/ln -s /usr/bin/python2.7 /usr/bin/python2
+#/bin/ln -s /usr/bin/python2.7 /usr/bin/python2
