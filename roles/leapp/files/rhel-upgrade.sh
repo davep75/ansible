1288f4a0629b30c437073bff612e9123f3abc3fb
davep75 <podolske@motorola.com>
2023-10-13 13:32:14 -0500

    commented commands

--- roles/leapp/files/rhel-upgrade.sh
+++ roles/leapp/files/rhel-upgrade.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
+#!/bin/bash -x
+# to save typing
+# subscription-manager status
+# subscription-manager list --consumed
+# subscription-manager repos --enable rhel-7-server-rpms
+# subscription-manager repos --enable rhel-7-server-extras-rpms
+# subscription-manager release unset
 /usr/bin/yum install -y leapp-upgrade 
 echo $?
-#leapp answer --section remove_pam_pkcs11_module_check.confirm=True
+# leapp answer --section remove_pam_pkcs11_module_check.confirm=True
+# modprobe -r pata_acpi
+# rpm -e --nodeps `rpm -qa | egrep 'el7\.' `
+
