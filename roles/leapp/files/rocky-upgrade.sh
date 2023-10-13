6ba55773ad700fd44dfc119f3cf961e44bb138d4
davep75 <podolske@motorola.com>
2023-10-13 13:32:29 -0500

    commented commands

--- roles/leapp/files/rocky-upgrade.sh
+++ roles/leapp/files/rocky-upgrade.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/bin/bash -x
 /usr/bin/yum install -y http://repo.almalinux.org/elevate/elevate-release-latest-el7.noarch.rpm
 echo $?
 /usr/bin/yum install -y leapp-upgrade leapp-data-rocky
 echo $?
-#leapp answer --section remove_pam_pkcs11_module_check.confirm=True
+# leapp answer --section remove_pam_pkcs11_module_check.confirm=True
+# modprobe -r pata_acpi
+#  rpm -e --nodeps `rpm -qa | egrep 'el7\.' `
